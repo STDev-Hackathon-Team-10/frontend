59bfda0dc58008fd0477153aae78abc8658009c6
lnylnylnylny <lnylnylnylny@naver.com>
2025-04-18 20:37:31 +0900

    홈화면 제작

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,31 @@
-import { Link } from "react-router";
+import { useNavigate } from "react-router-dom";
 import styles from "./Home.module.css";
+import logo from "../../Asset/logo.svg";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the Home page!</p>
-      <Link to="/game">Go to game page</Link>
+    <div className={styles.container}>
+      <div className={styles.logoBox}>
+        <img src={logo} alt="logo" className={styles.logoImg} />
+        <p>Mix different chemicals and discover new compounds!</p>
+      </div>
+
+      <div className={styles.inputBox}>
+        <div className={styles.nickname}>
+          <input placeholder="닉네임을 입력하세요" />
+        </div>
+        <div className={styles.group}>
+          <input placeholder="소속 그룹 (예: Team A)" />
+        </div>
+      </div>
+
+      <div className={styles.startbtnContainer}>
+        <button className={styles.startbtn} onClick={() => navigate("/game")}>
+          Let's Mix !
+        </button>
+      </div>
     </div>
   );
 }
