e535010e29de62c85ac7bd90197db51e18ad5b2f
나영 <160034314+lnylnylnylny@users.noreply.github.com>
2025-04-19 10:39:25 +0900

    Update CompoundModal.jsx

--- src/pages/Game/components/CompoundModal.jsx
+++ src/pages/Game/components/CompoundModal.jsx
@@ -1,12 +1,30 @@
 // src/pages/Game/components/CompoundModal.jsx
 import styles from "../Game.module.css";
+import { useEffect, useRef } from "react";
 
-export default function CompoundModal({ compound, onMove }) {
+export default function CompoundModal({ compound, onMove, onClose }) {
   if (!compound) return null;
+
+  const modalRef = useRef();
+
+  // 모달 바깥 클릭 시 닫기
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        onClose(); // 닫기 함수 호출
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [onClose]);
 
   return (
     <div className={styles.modalOverlay}>
-      <div className={styles.modalBox}>
+      <div className={styles.modalBox}  ref={modalRef}>
+                <button className={styles.closeBtn} onClick={onClose}>
+          ✕
+        </button>
         <p className={styles.compoundText}>{compound}</p>
         <p>발견했습니다!</p>
         <button className={styles.moveBtn} onClick={onMove}>
