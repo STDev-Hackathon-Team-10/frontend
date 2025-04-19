cf97c3f8c4b852333237f5c1c0955b5d812f4704
lnylnylnylny <lnylnylnylny@naver.com>
2025-04-19 13:24:11 +0900

    헤더 로고 전체 변경 및 수정

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./pages/Home";
+import Rank from "./pages/Rank";
+import Honor from "./pages/Honor";
+import Store from "./pages/Store";
 import Dictionary from "./pages/Dictionary/index.jsx";
 import Game from "./pages/Game/index.jsx";
 import AtomRenderer from "./components/Renderer";
@@ -23,6 +26,18 @@ let router = createBrowserRouter([
       {
         path: "dictionary",
         Component: Dictionary,
+      },
+      {
+        path: "honor",
+        Component: Honor,
+      },
+      {
+        path: "rank",
+        Component: Rank,
+      },
+      {
+        path: "store",
+        Component: Store,
       },
       {
         path: "test/renderer",
--- src/pages/Game/components/Header.jsx
+++ src/pages/Game/components/Header.jsx
@@ -1,10 +1,8 @@
 import styles from "../../Header/Header.module.css";
-import home from "../../../Asset/home.svg";
-import trophy from "../../../Asset/trophy.svg";
-import medal from "../../../Asset/medal.svg";
-import dictionary from "../../../Asset/dictionary.svg";
+import { IoStorefront, IoTrophySharp } from "react-icons/io5";
+import { FaMedal, FaBookOpen } from "react-icons/fa";
+import { HiArrowUturnLeft } from "react-icons/hi2";
 import mini_logo from "../../../Asset/mini_logo.svg";
-
 import { useNavigate } from "react-router";
 
 export default function Header() {
@@ -13,22 +11,14 @@ export default function Header() {
   return (
     <section className={styles.header}>
       <div className={styles.leftButtons}>
-        <div className={styles.tooltipWrapper} data-tooltip="홈">
-          <img
-            src={home}
-            alt="홈버튼"
-            onClick={() => navigate("/")}
-            className={styles.homeButton}
-          />
-        </div>
-        <div className={styles.tooltipWrapper} data-tooltip="도감">
-          <img
-            src={dictionary}
-            alt="도감버튼"
-            onClick={() => navigate("/dictionary")}
-            className={styles.homeButton}
-          />
-        </div>
+        <IoStorefront
+          onClick={() => navigate("/store")}
+          className={styles.headerButton}
+        />
+        <FaBookOpen
+          onClick={() => navigate("/dictionary")}
+          className={styles.headerButton}
+        />
       </div>
 
       <div className={styles.center}>
@@ -36,22 +26,15 @@ export default function Header() {
       </div>
 
       <div className={styles.rightButtons}>
-        <div className={styles.tooltipWrapper} data-tooltip="칭호">
-          <img
-            src={medal}
-            alt="칭호 버튼"
-            onClick={() => navigate("/honor")}
-            className={styles.headerButton}
-          />
-        </div>
-        <div className={styles.tooltipWrapper} data-tooltip="랭킹">
-          <img
-            src={trophy}
-            alt="트로피 버튼"
-            onClick={() => navigate("/rank")}
-            className={styles.headerButton}
-          />
-        </div>
+        <FaMedal
+          onClick={() => navigate("/honor")}
+          className={styles.headerButton}
+        />
+
+        <IoTrophySharp
+          onClick={() => navigate("/rank")}
+          className={styles.headerButton}
+        />
       </div>
     </section>
   );
